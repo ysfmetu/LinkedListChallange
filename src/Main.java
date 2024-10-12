c47bbc817cb89fc47f2fea40394a06f216a0f9e3
yusuf ülkü~ <ysf.metu@gmail.com>
2024-10-12 10:38:46 +0300

    ->Java 16 ile sunulan record sınıfını kullanarak Place adında bi sınıf ekledik amacımız dto listesi oluşturmak ve bunu taşımak ,bunun için ekstradan bir sınıf oluşturmak istemedik -> linkedList ile placeToVisit adında bir liste oluşturduk -> add metodu ile bu listeye yeni ve duplicate olmayan yerler ekliyoruz ->yer eklemesi yapıp gösterimini sağladık -> Place sınıfı için toString methodunu override yaptık ,gösterim şeklini değiştirdik

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.util.LinkedList;
+
+record Place(String name,int distance){
+    @Override
+    public String toString() {
+        return String.format(" %s (%d)", name,distance);
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        LinkedList<Place>placeToVisit=new LinkedList<>();
+        Place tokat=new Place("TOKAT",550);
+        add(placeToVisit,tokat);
+        System.out.println(placeToVisit);
+    }
+
+
+
+
+    private static void add(LinkedList<Place> list,Place place ){
+        if(list.contains(place)){
+            System.out.println("found duplicate place..."+place);
+            return;
+        }
+        list.add(place);
     }
 }
